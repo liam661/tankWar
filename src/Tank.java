f883c270bf6993efde442d108829a8753dd04377
liam <zhuchanglizq@126.com>
2022-02-17 17:19:32 +0800

    双缓存解决屏幕闪烁问题

--- src/Tank.java
+++ src/Tank.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.Collections;
 
 /**
  * @Description
@@ -36,7 +37,10 @@ public class Tank {
     }
 
     public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.YELLOW);
         g.fillRect(x,y,50,50);
+        g.setColor(c);
         move();
     }
 
