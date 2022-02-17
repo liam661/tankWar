5c2cb9bec3e5b9641bd1cb816e595b99ec320472
liam <zhuchanglizq@126.com>
2022-02-17 18:08:49 +0800

    消除子弹列表的内存泄漏问题。 小心处理迭代器中的删除问题，解决办法1：使用普通方式迭代；2：在迭代过程中删除

--- src/Bullet.java
+++ src/Bullet.java
@@ -6,19 +6,25 @@ import java.awt.*;
  * @Date 2022/2/17 16:44
  **/
 public class Bullet {
-    private static final int SPEED = 1;
+    private static final int SPEED = 10;
     private static final int WIDTH = 30;
     private static final int HEIGHT = 30;
     private int x,y;
     private Dir dir;
 
-    public Bullet(int x, int y, Dir dir){
+    private boolean live = true;
+    TankFrame tf = null;
+    public Bullet(int x, int y, Dir dir, TankFrame tankFrame){
         this.x = x;
         this.y = y;
         this.dir = dir;
+        this.tf = tankFrame;
     }
 
     public void paint(Graphics g){
+        if(!live){
+            tf.bullets.remove(this);
+        }
         Color c = g.getColor();
         g.setColor(Color.RED);
         g.fillOval(x, y, WIDTH, HEIGHT);
@@ -42,6 +48,12 @@ public class Bullet {
                 break;
             default:
                 break;
+        }
+        if(x < 0 || y < 0 ||
+                x > TankFrame.GAME_WIDTH
+                || y > TankFrame.GAME_HEIGHT){
+            live = false;
+
         }
     }
 }
--- src/TankFrame.java
+++ src/TankFrame.java
@@ -3,7 +3,9 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.security.Key;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description
@@ -12,7 +14,8 @@ import java.security.Key;
  **/
 public class TankFrame extends Frame {
     Tank myTank = new Tank(200, 200, Dir.DOWN, this);
-    Bullet b = new Bullet(300,300,Dir.DOWN);
+    //Bullet b = new Bullet(300,300,Dir.DOWN);
+    List<Bullet> bullets = new ArrayList<Bullet>();
     static final int GAME_WIDTH = 800;
     static final int GAME_HEIGHT = 600;
 
@@ -49,8 +52,14 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.WHITE);
+        g.drawString("子弹的数量："+ bullets.size(), 10, 60);
+        g.setColor(c);
         myTank.paint(g);
-        b.paint(g);
+        for(int i = 0;i < bullets.size();i++){
+            bullets.get(i).paint(g);
+        }
     }
 
     class MyKeyListener extends KeyAdapter{
