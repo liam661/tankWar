116b819ec73f16169914e29bb341b4e69dfd7d75
liam <zhuchanglizq@126.com>
2022-02-18 09:33:42 +0800

    子弹换成图片

--- src/tank/ResourceMgr.java
+++ src/tank/ResourceMgr.java
@@ -10,13 +10,17 @@ import java.awt.image.BufferedImage;
  **/
 public class ResourceMgr {
     public static BufferedImage tankL, tankU,tankR,tankD;
-
+    public static BufferedImage bulletL,bulletU,bulletR,bulletD;
     static {
         try {
             tankL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("tankL.gif"));
             tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("tankU.gif"));
             tankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("tankR.gif"));
             tankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("tankD.gif"));
+            bulletL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("bulletL.gif"));
+            bulletU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("bulletU.gif"));
+            bulletR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("bulletR.gif"));
+            bulletD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("bulletD.gif"));
         } catch (Exception e) {
             e.printStackTrace();
         }
