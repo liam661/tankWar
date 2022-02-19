72117847072632b4e35c963c146bccc871326589
liam <zhuchanglizq@126.com>
2022-02-19 15:45:49 +0800

    敌人坦克随机移动

--- src/tank/Tank.java
+++ src/tank/Tank.java
@@ -108,9 +108,16 @@ public class Tank {
             default:
                 break;
         }
-        if(random.nextInt(10) > 8){
+        if(this.group == Group.BAD && random.nextInt(100) > 95){
             this.fire();
         }
+        if(this.group == Group.BAD && random.nextInt(100) > 95){
+            randomDir();
+        }
+    }
+
+    private void randomDir() {
+        this.dir = Dir.values()[random.nextInt(4)];
     }
 
     public void fire() {
